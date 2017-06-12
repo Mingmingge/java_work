4baea22c6f46fa4e8fc77b6372a2e3e156600a16
Mingmingge <hlm_hulimin@163.com>
2017-06-12 15:42:34 +0800

    console InputstreamReader

---
+++ review/src/test/Test.java
@@ -0,0 +1,23 @@
+package test;
+import java.io.*;
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		InputStreamReader sr=new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(sr);
+		String str=null;
+		while(true){
+			str=br.readLine();
+			if(!("ok".equals(str))){
+				System.out.println(str+"  ");
+				
+			}
+			else{
+				System.out.println("您确定输入完成了吗？");
+				System.exit(0);
+			}
+		}
+		
+	}
+
+}
