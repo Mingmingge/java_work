244248b1ec6f1ed1a52ce8b59d7d534f20f471de
Mingmingge <hlm_hulimin@163.com>
2017-06-12 14:26:57 +0800

    The Last

---
+++ review/src/t_4/T4.java
@@ -0,0 +1,19 @@
+package t_4;
+
+public class T4 {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for(int i = 5;i<=8;i++)
+			for(int j = 0; j <=6; j++)
+				for(int k = 0; k <=30; k++)
+				{
+					if(100*i + 50 * j + 10 * k == 800)
+					{
+						num++;
+						System.out.println("100元：" + i + "，50元：" + j + "，10元：" + k);
+					}
+				}
+		System.out.println("共" + num + "种组合情况");
+	}
+}
---
+++ review/src/t_9/T9.java
@@ -0,0 +1,54 @@
+package t_9;
+
+import java.util.HashSet;
+
+class  Worker
+{
+	private String name;
+	private int age;
+	private double salary;
+	
+	public Worker(String name,int age,double salary)
+	{
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+	
+	public String toString()
+	{
+		return name + "," + age + "," + salary;
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode() + 39 * age;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Worker other = (Worker) obj;
+		return this.name.equals(other.name) && this.age == other.age;
+	}	
+}
+
+public class T9 {
+
+	public static void main(String[] args) {
+		HashSet<Worker> hset = new HashSet<Worker>();
+		hset.add(new Worker("w1", 21, 2000));
+		hset.add(new Worker("w2", 22, 2200));
+		hset.add(new Worker("w3", 20, 5000));
+		hset.add(new Worker("w1", 21, 2000));
+		hset.add(new Worker("w2", 22, 2200));
+		
+		for(Worker w : hset)
+			System.out.println(w);
+	}
+}
