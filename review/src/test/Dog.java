0dbfc2f0e440b6f846fceb2fd9652649de68b4d4
Mingmingge <hlm_hulimin@163.com>
2017-06-13 12:08:35 +0800

    11

--- review/src/test/Dog.java
+++ review/src/test/Dog.java
@@ -1,11 +1,13 @@
 package test;
 
 public class Dog extends Animal {
-	int b=0;
+	static int b=0;
 	public Dog(){
+		jiao();
 		b=1000;
 	}
 	public static void jiao(){
+		System.out.println("b="+b);
 		System.out.println("I am a dog!");
 	}
 
--- review/src/test/Test_5.java
+++ review/src/test/Test_5.java
@@ -2,7 +2,10 @@ package test;
 
 public class Test_5 {
 	public static void main(String args[]){
+		
 		Dog dog=new Dog();
+		Animal.jiao();
+		Dog.jiao();
 	}
 
 }
