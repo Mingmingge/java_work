ae632bd44a04783dcc50c0d6c5eca39090794b39
Mingmingge <hlm_hulimin@163.com>
2017-06-13 11:55:27 +0800

    no

---
+++ review/src/test/Test_4.java
@@ -0,0 +1,14 @@
+package test;
+import java.util.*;
+public class Test_4 {
+
+	public static void main(String[] args) {
+		Scanner reader=new Scanner(System.in);
+		System.out.println("请输入一个整型数据");
+		int a=reader.nextInt();
+		System.out.println(a);
+		
+
+	}
+
+}
